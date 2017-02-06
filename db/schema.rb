c6e8ba46bece78e43b40ae65e51c162dac69379a
5-5-C <jmarshall.brock@gmail.com>
2017-02-06 16:59:45 -0500

    Generating the products controller and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206194505) do
+ActiveRecord::Schema.define(version: 20170206215603) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20170206194505) do
     t.integer  "price_in_cents"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.string   "image_url"
   end
 
 end
