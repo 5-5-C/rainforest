5d3b53d44636d3dbca769e4ccc9c53b5eee6ddf5
5-5-C <jmarshall.brock@gmail.com>
2017-02-06 17:12:49 -0500

    Writing product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,4 @@
 class Product < ApplicationRecord
   validates :description, :name, presence: true
-  validates :price_in_cents, numerically: {only_integer: true}
+  validates :price_in_cents, numericality: {only_integer: true}
 end
